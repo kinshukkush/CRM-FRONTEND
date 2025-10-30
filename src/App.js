767e03b92c6233310b818ca5e2e5b57f034a8850
kinshukkush <kinshuksaxena3@gmail.com>
2025-10-30 18:43:20 +0530

    Add /home route to fix OAuth redirect

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
 
         <Route
